31e6d5e924efa5c23b1496b5ade9d1c68b98691d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-04-08 20:42:35 -0400

    default.nix: Add bootloader output
    Co-authored-by: Cosima Neidahl <opna2608@protonmail.com>

--- default.nix
+++ default.nix
@@ -1,6 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
 
 {
+  bootloader = pkgs.callPackage (
+    { stdenv
+    , lib
+    , grub2_efi
+    }:
+    stdenv.mkDerivation {
+      name = "nixos-grub-installer-theme";
+      srcs = lib.cleanSource ./.;
+      sourceRoot = "source/bootloader";
+      nativeBuildInputs = [ grub2_efi ];
+      makeFlags = [ "prefix=$(out)" ];
+    }
+  ) {};
   icons = pkgs.callPackage (
     { stdenv
     , lib
